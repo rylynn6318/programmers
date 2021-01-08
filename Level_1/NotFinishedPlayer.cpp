d93903646411e0afa432561080a3a84b7dc5ecad
동현 박 <gg6318@naver.com>
2021-01-08 15:24:47 +0900

    success

---
+++ Level_1/NotFinishedPlayer.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    map<string, int> pMap;
+
+    for (const auto& p : participant)
+    {
+        if (pMap.find(p) == pMap.end()) // 없을 때
+        {
+            pMap.insert({ p, 1 });
+        }
+        else
+        {
+            pMap.find(p)->second++;
+        }
+    }
+
+    for (const auto& c : completion)
+    {
+        pMap.find(c)->second--;
+    }
+
+    for (const auto& p : pMap)
+    {
+        if (p.second == 1)
+            return p.first;
+    }
+}
