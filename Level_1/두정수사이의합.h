da869facaf0fe99917e4f398e7035d4f4657ce01
동현 박 <gg6318@naver.com>
2021-01-15 14:51:05 +0900

    success

---
+++ Level_1/두정수사이의합.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    if (a == b) return a;
+    int min = std::min(a, b);
+    int max = std::max(a, b);
+    while (min <= max)
+        answer += min++;
+
+    return answer;
+}
