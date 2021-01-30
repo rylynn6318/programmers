8a4d1136e3d67f028f1a7bbcc858b9b1c333eaaf
동현 박 <gg6318@naver.com>
2021-01-30 17:58:36 +0900

    success

---
+++ Programmers_Level_2/PhoneBook.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    std::sort(phone_book.begin(), phone_book.end());
+    for (int i = 0; i < phone_book.size() - 1; i++)
+    {
+        if (phone_book[i + 1].find(phone_book[i]) == 0)
+        {
+            return false;
+            break;
+        }
+    }
+    return true;
+}
