cc3e68d3de83eea077c23a9f905c4444ba892390
동현 박 <gg6318@naver.com>
2021-05-13 02:42:37 +0900

    success

---
+++ Programmers_Level_2/MakeBigNumber.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+	string answer = "";
+	int index = -1;
+	for (int i = 0; i < number.size() - k; i++)
+	{
+		char max = 0;
+		for (int j = index + 1; j <= k + i; j++) 
+		{
+			if (max < number[j]) 
+			{
+				index = j;
+				max = number[j];
+			}
+		}
+		answer += max;
+	}
+	return answer;
+}
+
